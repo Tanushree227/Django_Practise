573afb16f120d440d05cf2d74da0f1ac1f16b71e
Tanushree227 <tanushreedas813@gmail.com>
2024-02-26 15:39:44 +0530

    django preparing template

--- hakuna_/views.py
+++ hakuna_/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-  template = loader.get_template('index.html')
-  return HttpResponse(template.render())
+  mypeoples = Member.objects.all().values()
+  template = loader.get_template('peoples.html')
+  context = {
+    'mypeoples': mypeoples,
+  }
+  return HttpResponse(template.render(context, request))
